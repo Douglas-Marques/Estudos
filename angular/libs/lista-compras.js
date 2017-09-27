62caafb1f23988b32c533ebdf763ed5e4c03c79d
Eduardo Dornel Ribas <EDRIBAS@edu.unisinos.br>
2017-09-26 21:12:26 -0300

    evoluir crud de itens

--- angular/libs/lista-compras.js
+++ angular/libs/lista-compras.js
@@ -3,13 +3,16 @@ function ListaComprasController($scope) {
 		{produto: 'Leite', quantidade: 2, comprado: false},
 		{produto: 'Cerveja', quantidade: 12, comprado: false}
 	];
+	
+	$scope.flag_editar = false;
+	
+	$scope.nomeNovo = '';
 
 	$scope.adicionaItem = function () {
 		let item = {
 			produto : $scope.item.produto,
 			quantidade: $scope.item.quantidade,
 			comprado: false,
-			id: $scope.idAtual++
 		};
 		if(validarCamposObrigatorios(item)){
 			$scope.itens.push(item);
@@ -20,8 +23,27 @@ function ListaComprasController($scope) {
 	};
 
 	$scope.editarItem = function(item){
+		$scope.flag_editar = true;
 		let elementoProduto = document.getElementById(item.produto);
-		elementoProduto.innerHTML = '<input type="text" ng-model="item.produto" placeholder="Produto" value='+item.produto+' required="true">';
+		elementoProduto.innerHTML = '<input type="text" ng-model="nomeNovo" placeholder="Produto" value='+item.produto+' required="true">';
+	}
+	
+	$scope.salvarEdicao = function(item){
+		$scope.flag_editar = false;
+		let len = $scope.itens.length;
+		for(let i = 0; i < len; i++){
+			if($scope.itens[i].produto === item.produto){
+				atualizarHtml(item, $scope.nomeNovo);
+				$scope.itens[i].produto = $scope.nomeNovo;
+				return;
+			}
+		}
+	}
+	
+	function atualizarHtml(item, nomeNovo){
+		let elementoProduto = document.getElementById(item.produto);
+		elementoProduto.id = $scope.nomeNovo;
+		elementoProduto.innerHTML = '<strong>'+nomeNovo+'</strong>'
 	}
 
 	$scope.removerItem = function(item){
@@ -29,6 +51,7 @@ function ListaComprasController($scope) {
 		$scope.itens.splice(index, 1);
 	}
 }
+
 
 function limparInputs(escopo){
 	escopo.item.produto = '';
